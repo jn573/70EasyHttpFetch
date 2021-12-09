e9e8f37c7f890fdc3fa7019fee3e297a770edbe4
James <cheekywena@gmail.com>
2021-12-09 17:58:39 +0200

    read from file and remove api key

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const http = new EasyHTTP()
 
 const data = {
+    "id": 2,
     name: 'Evan Badass',
     username: 'Glock19x',
     email: 'cuteness@gmail.com'
 }
+
+// http.get('users.json', data)
+//     .then(data => console.log(data))
+//     .then(err => console.log(err))
+
+http.post('users.json', data)
+    .then(data => console.log(data))
+    .then(err => console.log(err))
 
 // http.get('https://jsonplaceholder.typicode.com/users', data)
 //     .then(data => console.log(data))
@@ -18,6 +27,7 @@ const data = {
 //     .then(data => console.log(data))
 //     .then(err => console.log(err))
 
-http.delete('https://jsonplaceholder.typicode.com/users/2')
-    .then(data => console.log(data))
-    .then(err => console.log(err))
+// http.delete('https://jsonplaceholder.typicode.com/users/2')
+//     .then(data => console.log(data))
+//     .then(err => console.log(err))
+
